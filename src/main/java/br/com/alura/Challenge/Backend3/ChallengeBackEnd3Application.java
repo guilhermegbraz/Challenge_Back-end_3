f5cfa4ec1cd8670d379d4795fc28a168262b9be8
Gui <guilherme.g.braz@outlook.com>
2022-09-21 17:58:11 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/Challenge/Backend3/ChallengeBackEnd3Application.java
@@ -0,0 +1,13 @@
+package br.com.alura.Challenge.Backend3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackEnd3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackEnd3Application.class, args);
+	}
+
+}
